efcd3ce5c853ee8ed0db2bb9f29afeb9d3fa9509
hrishimathur19 <rishimathur1019@gmail.com>
2026-08-28 19:03:38 +0530

    Add LeetCode 26_removeduplicatefromsortedarray

---
+++ 26_removeduplicatefromsortedarray.cpp
@@ -0,0 +1,31 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+         int unique = 1;
+        int officer = 1;
+        int cm = 1;
+        int n = nums.size();
+
+        while(cm < n)
+        {
+            if(nums[cm] == nums[cm - 1])
+            {
+                cm++;
+                continue;
+            }
+
+            nums[officer] = nums[cm];
+
+            officer++;
+            unique++;
+            cm++;
+        }
+
+        return unique;
+    
+        
+    }
+};  
